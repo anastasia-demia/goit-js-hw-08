8c36ec058c1693c90589e11f57cb0e116caa590c
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-03-08 18:07:54 +0200

    start

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,5 @@
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
