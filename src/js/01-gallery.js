fc04d853525ff6ec4feb9af306dfdd89bcf9852d
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-03-09 14:34:56 +0200

    not fully done task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,40 @@
 import SimpleLightbox from "simplelightbox";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', itemsMarkup)
+galleryContainer.addEventListener("click", onImageClick)
+
+function createGalleryItems (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+        </div>`
+    }).join('')
+};
+
+function onImageClick(event) {
+    event.preventDefault()
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData:"alt",
+    captionPosition:'bottom',
+    captionDelay: 250,
+});
