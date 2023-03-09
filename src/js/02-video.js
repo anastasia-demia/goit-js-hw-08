a8c4f481ff702a812b38478c290eba58fd50068e
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-03-09 19:25:20 +0200

    task 2 progress update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const timeKey = "videoplayer-current-time";
+
+
+
+const onPlay = function(data) {
+  //TODO data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+
+
+
+
+const savedTime = localStorage.getItem(timeKey)
+
+player.setCurrentTime(savedTime).then(function(seconds) {
+}).catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          break;
+
+      default:
+          break;
+  }
+});
