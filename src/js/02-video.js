81e88dc8ae20a360a063461122528b0274656e0e
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-03-10 19:39:13 +0200

    task 2 update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,15 @@
+import Player from '@vimeo/player';
 import throttle from "lodash.throttle";
 
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-const timeKey = "videoplayer-current-time";
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const timeKey = 'videoplayer-current-time';
 
-const onPlay = function(data) {
-  localStorage.setItem(timeKey, JSON.stringify(data));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(timeKey)) || 0);
+
+function onPlay(data) {
+  localStorage.setItem(timeKey, JSON.stringify(data.seconds));
 };
 
 player.on('timeupdate',throttle(onPlay, 1000));
-
-const savedTime = localStorage.getItem(timeKey)
-
-player.setCurrentTime(savedTime).then(function(seconds) {
-}).catch(function(error) {
-  switch (error.name) {
-      case 'RangeError':
-          break;
-
-      default:
-          break;
-  }
-});
